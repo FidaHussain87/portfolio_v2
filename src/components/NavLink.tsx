512e07db4693a4ed28f244f973f289956a810a33
FidaHussain87 <36170696+FidaHussain87@users.noreply.github.com>
2024-04-13 19:51:47 +0200

    home screen content completed

---
+++ src/components/NavLink.tsx
@@ -0,0 +1,22 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const NavLink = ({ link }: any) => {
+  const pathName = usePathname();
+  console.log("PathName: ", pathName);
+  return (
+    <Link
+      className={` rounded p-1 ${
+        pathName === link.url && "bg-[#0F172A] text-white "
+      }  `}
+      href={link.url}
+    >
+      {link.title}
+    </Link>
+   
+  );
+};
+
+export default NavLink;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { FaGithub } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
+import NavLink from "./NavLink";
 
 const menus = [
   { url: "/", title: "Home" },
@@ -21,23 +22,21 @@ const Navbar = () => {
     setOpen(!open);
   };
   return (
-    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
       {/* LINKS */}
       <div className="hidden md:flex gap-4 w-1/3">
         {menus.map((link) => (
-          <Link href={link.url} key={link.title}>
-            {link.title}
-          </Link>
+          <NavLink link={link} key={link.title} />
         ))}
       </div>
       {/* logo */}
-      <div className=" md:hidden lg:flex  w-1/3">
+      <div className=" md:hidden lg:flex  xl:w-1/3 xl:justify-center">
         <Link
           href="/"
-          className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
+          className="text-sm bg-[#0F172A] rounded-md p-1 font-semibold flex items-center justify-center"
         >
           <span className="text-white mr-1">Fida</span>
-          <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">
+          <span className="w-12 h-8 rounded bg-white text-[#0F172A] flex items-center justify-center">
             .dev
           </span>
         </Link>
@@ -48,10 +47,10 @@ const Navbar = () => {
           <FaGithub size={24} />
         </Link>
         <Link href="/">
-        <FaFacebook  size={24} color="#1877F2"/>
+          <FaFacebook size={24} color="#1877F2" />
         </Link>
         <Link href="/">
-        <FaLinkedin  size={24} color="#0077b5"/>
+          <FaLinkedin size={24} color="#0077b5" />
         </Link>
       </div>
       {/* RESPONSIVE Menu */}
@@ -60,13 +59,13 @@ const Navbar = () => {
           className="w-10 h-8 flex flex-col justify-between z-50 relative"
           onClick={handleMenu}
         >
-          <div className="w-10 h-1 bg-black rounded"></div>
-          <div className="w-10 h-1 bg-black rounded"></div>
-          <div className="w-10 h-1 bg-black rounded"></div>
+          <div className="w-10 h-1 bg-[#0F172A] rounded"></div>
+          <div className="w-10 h-1 bg-[#0F172A] rounded"></div>
+          <div className="w-10 h-1 bg-[#0F172A] rounded"></div>
         </button>
         {/* Menu List */}
         {open && (
-          <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+          <div className="absolute top-0 left-0 w-screen h-screen bg-[#0F172A] text-white flex flex-col items-center justify-center gap-8 text-4xl">
             {menus?.map((link) => (
               <Link href={link.url} key={link.title}>
                 {link.title}
