618c925498949ec3feda68adcb41e17a04369912
FidaHussain87 <36170696+FidaHussain87@users.noreply.github.com>
2024-04-22 00:37:01 +0200

    portfolio page completed v1

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,7 +1,50 @@
-'use client';
-import React from "react";
-import { motion } from "framer-motion";
+"use client";
+import React, { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const items = [
+  {
+    id: 1,
+    color: "from-red-300 to-blue-300",
+    title: "React Commerce",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    link: "https://lama.dev",
+  },
+  {
+    id: 2,
+    color: "from-blue-300 to-violet-300",
+    title: "Next.js Medium Blog",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    link: "https://lama.dev",
+  },
+  {
+    id: 3,
+    color: "from-violet-300 to-purple-300",
+    title: "Vanilla Book App",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    link: "https://lama.dev",
+  },
+  {
+    id: 4,
+    color: "from-purple-300 to-red-300",
+    title: "Spotify Music App",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    link: "https://lama.dev",
+  },
+];
+
 const PortfolioPage = () => {
+  const portfolioRef: any = useRef();
+  const { scrollYProgress } = useScroll({ target: portfolioRef });
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "-80%"]);
+
   return (
     <motion.div
       className="h-full"
@@ -9,7 +52,74 @@ const PortfolioPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      PortfolioPage
+      <div className="h-[600vh] relative" ref={portfolioRef}>
+        <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">
+          My Work
+        </div>
+        <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
+          <motion.div style={{ x }} className="flex">
+            <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-purple-300 to-red-300" />
+            {items?.map((item) => (
+              <div
+                key={item.id}
+                className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${item.color}`}
+              >
+                <div className="flec flex-col gap-8 text-white">
+                  <h1 className="text-xl md:text-4xl lg:text-6xl xl:text-8xl font-bold">
+                    {item.title}
+                  </h1>
+                  <div className="relative w-80 h-56 md:w-96 md:h-64 lg:w-[500px] lg:h-[350px] xl:w-[600px] xl-h-[420px]">
+                    <Image src={item.img} alt="" fill  className="rounded-b-lg rounded-s-lg"/>
+                  </div>
+                  <p className="w-80 md:w-96 lg:w-[500px] xl:w-[600px] xl:text-xl">
+                    {item.desc}
+                  </p>
+                  <Link
+                    href={item.link}
+                    className="flex justify-end text-black"
+                  >
+                    <Button
+                      className="text-sm md:text-md lg:text-lg xl:text-xl font-semibold text-gray-600"
+                      variant="outline"
+                    >
+                      See Demo
+                    </Button>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </motion.div>
+        </div>
+      </div>
+      <div className="w-screen h-screen flex flex-col  items-center justify-center text-center mt-10 ">
+        <h1 className="text-8xl">Do You have a Project?</h1>
+        <div className="relative">
+          <motion.svg
+            animate={{ rotate: 360 }}
+            transition={{ duration: 8, ease: "linear", repeat: Infinity }}
+            viewBox="0 0 300 300"
+            className="w-64 h-64 md:w-[500px] md:h-[500px] "
+          >
+            <defs>
+              <path
+                id="circlePath"
+                d="M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "
+              />
+            </defs>
+            <text fill="#000">
+              <textPath xlinkHref="#circlePath" className="text-xl">
+                Full Stack Developer with focus on NextJs
+              </textPath>
+            </text>
+          </motion.svg>
+          <Link
+            href="/contact"
+            className="w-16 h-16 md:w-28 md:h-28 absolute top-0 cursor-pointer bottom-0 left-0 right-0 m-auto bg-black text-white rounded-full flex items-center justify-center"
+          >
+            Hire Me
+          </Link>
+        </div>
+      </div>
     </motion.div>
   );
 };
